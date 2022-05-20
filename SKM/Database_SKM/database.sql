2931d0baf643079b24f080b39a8616bf9bb4d2f0
apwbs <edoardo.marangone@uniroma1.it>
2022-05-20 19:21:25 +0200

    remove local databases

--- SKM/Database_SKM/database.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE keys (  
-    requester_address TEXT, 
-    generated_key_dumped TEXT,
-    primary key (requester_address, generated_key_dumped)
-);
-
--- SKM/Database_SKM/users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users_data (  
-    user_address TEXT,
-    attributes TEXT,
-    primary key (user_address, attributes)
-);
-
